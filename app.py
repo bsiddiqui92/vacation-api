78adfe89a15ef74c5793b343ce4d709bbc4bbdb3
bilal <bos19992@gmail.com>
2017-07-31 23:34:05 -0400

    add to requirements

--- app.py
+++ app.py
@@ -20,5 +20,4 @@ api.add_resource()
 if __name__ == '__main__':
 	from db import db
 	db.init_app(app)
-
-app.run(port=5000, debug=True)  # important to mention debug=True
+	app.run(port=5000, debug=True)  # important to mention debug=True
