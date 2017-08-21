a650e83464aa429e66557577cbd91bdac32250c6
bilal <bos19992@gmail.com>
2017-08-21 01:02:32 -0400

    all

--- resources/request.py
+++ resources/request.py
@@ -125,9 +125,11 @@ class PendingRequests(Resource):
     def get(self, employee_id):
         try:
             if employee_id:
-                result = RequestModel.get_employee_vacation('pending', employee_id)
+                return {"data": 'biatch'}, 200
+                result = RequestModel.get_vacation('pending', employee_id)
             else:
-                result = RequstModel.get_employee_vacation('pending')
+                return {"data": 'biatch is a no go'}, 200
+                result = RequstModel.get_vacation('pending')
 
             return { "data" : result}, 200
         except Exception as error:
